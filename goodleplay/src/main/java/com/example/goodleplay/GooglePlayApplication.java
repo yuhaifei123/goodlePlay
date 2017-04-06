7dd5be01160358ca012be39506d39cad7c504a7d
yuhaifei <1129639939@qq.com>
2017-04-06 21:40:19 +0800

    s

--- goodleplay/src/main/java/com/example/goodleplay/GooglePlayApplication.java
+++ goodleplay/src/main/java/com/example/goodleplay/GooglePlayApplication.java
@@ -11,8 +11,8 @@ import android.os.Process;
 public class GooglePlayApplication extends Application{
 
 	private  static Context context;
-	private Handler mHandler;
-	private int mI;
+	private static Handler mHandler;
+	private static int mI;
 
 	@Override
 	public void onCreate() {
@@ -20,6 +20,17 @@ public class GooglePlayApplication extends Application{
 		context = getApplicationContext();
 		mHandler = new Handler();
 		mI = Process.myPid();//当前主线程
+	}
 
+	public static Context getContext() {
+		return context;
+	}
+
+	public static Handler getmHandler() {
+		return mHandler;
+	}
+
+	public static int getmI() {
+		return mI;
 	}
 }
